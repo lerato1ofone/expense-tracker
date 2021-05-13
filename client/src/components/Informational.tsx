ff54c4f95a8bef37b878c19eb746b02be26aa589
lerato1ofone <letsepe100@gmail.com>
2021-05-13 22:24:17 +0200

    Added dummy content on informational component

--- client/src/components/Informational.tsx
+++ client/src/components/Informational.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function Informational() {
     return (
-        <div>
-            
+        <div className="h-full">
+            <div></div>
+            <div>
+                <h2>Show your money who’s boss! </h2>
+                <p>See what expenses use up most of your money and fire those expenses if you wish, you are the boss afterall.</p>
+                <button>Sign up. It’s free.</button>
+            </div>
+            <div className="">
+                <h3>Benefits</h3>
+                <p>Over half a million users like us. <span>&#8594;</span></p>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                </div>
+            </div>
         </div>
     )
 }
