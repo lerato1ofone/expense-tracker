618ea8ebf4e08856d87c61ca53791b8aee967b21
lerato1ofone <letsepe100@gmail.com>
2021-05-13 22:24:17 +0200

    Added informational component in main app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
+import Informational from './components/Informational';
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-purple via-light-white to-transaparent h-screen">
       <Navigation />
       <Hero />
+      <Informational />
     </div>
   );
 }
