910a211f8fa1cfee3a2978e346bd6f8f0f210e98
lerato1ofone <letsepe100@gmail.com>
2021-05-13 22:24:17 +0200

    Added all information content except svgs, styling

--- client/src/components/Informational.tsx
+++ client/src/components/Informational.tsx
@@ -4,26 +4,28 @@ export default function Informational() {
     return (
         <div className="h-full">
             <div></div>
-            <div>
-                <h2>Show your money who’s boss! </h2>
-                <p>See what expenses use up most of your money and fire those expenses if you wish, you are the boss afterall.</p>
-                <button>Sign up. It’s free.</button>
-            </div>
-            <div className="">
-                <h3>Benefits</h3>
-                <p>Over half a million users like us. <span>&#8594;</span></p>
+            <div className="w-4/5 m-auto">
                 <div>
+                    <h2>Show your money who’s boss! </h2>
+                    <p>See what expenses use up most of your money and fire those expenses if you wish, you are the boss afterall.</p>
+                    <button>Sign up. It’s free.</button>
+                </div>
+                <div className="">
+                    <h3>Benefits</h3>
+                    <p>Over half a million users like us. <span>&#8594;</span></p>
                     <div>
-                        <p>Secure and reliable, only you get access to your account and make changes.</p>
-                    </div>
-                    <div>
-                        <p>See exactly where your money goes & keep track of it.</p>
-                    </div>
-                    <div>
-                        <p>Your transactions are organized in a categoried fashion.</p>
-                    </div>
-                    <div>
-                        <p>Don’t spend money to understand your money. Get everything for free.</p>
+                        <div>
+                            <p>Secure and reliable, only you get access to your account and make changes.</p>
+                        </div>
+                        <div>
+                            <p>See exactly where your money goes & keep track of it.</p>
+                        </div>
+                        <div>
+                            <p>Your transactions are organized in a categoried fashion.</p>
+                        </div>
+                        <div>
+                            <p>Don’t spend money to understand your money. Get everything for free.</p>
+                        </div>
                     </div>
                 </div>
             </div>
