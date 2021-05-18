13c635a3a3598b73fb872f568cb1f6de5c607228
lerato1ofone <letsepe100@gmail.com>
2021-05-18 22:24:17 +0200

    Updated Informational styling (flex)

--- client/src/components/Informational.tsx
+++ client/src/components/Informational.tsx
@@ -12,13 +12,13 @@ export default function Informational() {
                 <Info />
             </div>
             
-            <div className="w-4/5 m-auto relative pt-28">
+            <div className="w-4/5 m-auto relative pt-28 h-4/5 flex flex-col justify-between">
                 <div>
                     <h2 className="font-bold text-4xl w-1/4 tracking-wide leading-tighter mb-6">Show your money who’s boss! </h2>
                     <p className="mb-6 font-semibold">See what expenses use up most of your money and fire those expenses if you wish, you are the boss afterall.</p>
                     <button className="bg-green p-3 rounded font-semibold text-white tracking-wide">Sign up. It’s free.</button>
                 </div>
-                <div className="flex flex-col items-center mt-12">
+                <div className="flex flex-col items-center">
                     <h3 className="text-2xl font-semibold mb-6">Benefits</h3>
                     <p>Over half a million users like us. <span>&#8594;</span></p>
                     <div className="flex mt-12 break-words">
