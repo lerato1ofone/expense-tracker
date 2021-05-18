1114d11befd596539cf4b0f250e3d4ce5a348337
lerato1ofone <letsepe100@gmail.com>
2021-05-18 22:24:17 +0200

    Added content on HowItWorks section

--- client/src/components/HowItWorks.tsx
+++ client/src/components/HowItWorks.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Computer from '../icons/Computer'
 
 export default function HowItWorks() {
     return (
         <div>
-            
+            <div>
+                <h3>How it works</h3>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div></div>
+                        <p>Register your account, and sign into your account.</p>
+                    </div>
+                    <div>
+                        <div></div>
+                        <p>Define some categories, or add a transaction directly.</p>
+                    </div>
+                    <div>
+                        <div></div>
+                        <p>See how much your are spending..</p>
+                    </div>
+                    <div>
+                        <div></div>
+                        <p>Curate a budget and tailer your spending to meet your needs.</p>
+                    </div>
+                </div>
+                <Computer />
+            </div>
         </div>
     )
 }
