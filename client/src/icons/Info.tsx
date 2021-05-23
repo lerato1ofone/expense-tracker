2a10b7ccac7d13a15ab3ac03deee5950abad1a55
lerato1ofone <letsepe100@gmail.com>
2021-05-23 22:24:17 +0200

    Added info svg component

---
+++ client/src/icons/Info.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Info() {
+    return (
+        <svg width="1440" height="587" viewBox="0 0 1440 587" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d)">
+            <path d="M-3 579V0H1443V173V269V365C1443 365 1417.5 405.5 1395 406.5C1372.5 407.5 1332 435 1308.5 435C1285 435 1237 486.5 1141 474C1045 461.5 1063.5 486.5 1002.5 503.5C941.5 520.5 886 486.5 845 486.5C804 486.5 753 461.5 697 474C641.627 486.36 597.498 470.366 563.163 457.921L562 457.5C527.5 445 467.5 435 467.5 435C467.5 435 368.5 426 304 486.5C239.5 547 227.5 542 201.5 547C175.5 552 86.672 570.615 -3 579Z" fill="#D0D5F2"/>
+            </g>
+            <defs>
+            <filter id="filter0_d" x="-7" y="0" width="1454" height="587" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+            </filter>
+            </defs>
+        </svg>
+    )
+}
