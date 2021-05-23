3990098472bb1c79c9f4963a5dcfe7070fcc575a
lerato1ofone <letsepe100@gmail.com>
2021-05-23 22:24:17 +0200

    Added eye icon

---
+++ client/src/icons/EyeIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function EyeIcon() {
+    return (
+        <svg width="46" height="41" viewBox="0 0 46 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22.909 36.211C27.384 36.211 31.0117 32.7997 31.0117 28.5917C31.0117 24.3836 27.384 20.9723 22.909 20.9723C18.434 20.9723 14.8063 24.3836 14.8063 28.5917C14.8063 32.7997 18.434 36.211 22.909 36.211Z" stroke="#13C6B9" stroke-width="2.1384"/>
+            <path d="M23.1138 30.0012C23.8195 30.0012 24.3916 29.4613 24.3916 28.7954C24.3916 28.1294 23.8195 27.5895 23.1138 27.5895C22.4081 27.5895 21.836 28.1294 21.836 28.7954C21.836 29.4613 22.4081 30.0012 23.1138 30.0012Z" fill="#13C6B9" stroke="#13C6B9" stroke-width="2.1384"/>
+            <path d="M33.6974 4.87744C33.6974 9.54038 29.7232 13.3204 24.8208 13.3204" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M33.925 4.87744C33.925 9.54038 37.8992 13.3204 42.8016 13.3204" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M33.6974 21.8816C33.6974 17.2187 29.7232 13.4386 24.8208 13.4386" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M33.925 21.8816C33.925 17.2187 37.8992 13.4386 42.8016 13.4386" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.6102 2.10254C20.6102 5.21116 18.0117 7.7312 14.8063 7.7312" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.6102 2.10254C20.6102 5.21116 23.2087 7.7312 26.4141 7.7312" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.6102 13.4681C20.6102 10.3595 18.0117 7.83948 14.8063 7.83948" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.6102 13.4681C20.6102 10.3595 23.2087 7.83948 26.4141 7.83948" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M29.4575 18.883C27.361 18.3629 25.2074 18.1029 22.9968 18.1029C15.2013 18.1029 8.11489 21.3365 1.73748 27.8038C1.28805 28.2595 1.28805 28.9652 1.73748 29.421C8.11489 35.8882 15.2013 39.1218 22.9968 39.1218C30.7608 39.1218 37.8215 35.9143 44.1788 29.4992C44.6537 29.02 44.6267 28.2686 44.1185 27.8209C44.1146 27.8174 44.1106 27.8139 44.1066 27.8105C41.0749 25.2071 38.8758 23.5046 37.5092 22.7031" stroke="#13C6B9" stroke-width="2.1384" stroke-linecap="round"/>
+        </svg>
+    )
+}
