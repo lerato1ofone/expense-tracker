dbbead9871a7740884194a6240ed876e96b654cf
lerato1ofone <letsepe100@gmail.com>
2021-06-07 22:24:17 +0200

    Added hero section to main App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-purple via-light-white to-transaparent h-screen">
       <Navigation />
+      <Hero />
     </div>
   );
 }
