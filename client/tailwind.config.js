7f0ba3bf2db5d6f7f51a4889b77997f5128261c6
lerato1ofone <letsepe100@gmail.com>
2021-08-03 16:43:32 +0200

    Added tailwind configuration and imported poppins and montserrat fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,43 @@
-const colors = require('tailwindcss/colors')
 module.exports = {
-   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-      colors: {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
       transparent: 'transparent',
-      current: 'currentColor'
-      ....
-    }
+      current: 'currentColor',
+      purple: '#D0D5F2',
+      'dark-purple': '#AB62FE',
+      green: '#13C6B9',
+      white: '#FFF',
+      'dark-white': '#DFDFDF',
+      'light-white': '#F2F3F9',
+      'light-black': '#0D1117'
     },
-    variants: {
-      extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
-    plugins: [
-      require('@tailwindcss/forms'), // import tailwind forms
-   ],
-  }
+    screens: {
+      'sm': '500px',
+      'md': '768px',     
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem'
+      },
+      fontFamily: {
+        body: ['Montserrat', 'sans-serif'],
+        logo: ['Poppins', 'sans-serif']
+      }      
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [
+  ],
+}
