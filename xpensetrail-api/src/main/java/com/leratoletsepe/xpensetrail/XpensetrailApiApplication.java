39775675b36c12ba3986c2ae2b6eda9eef52ade0
Lerato1OFONE <letsepe100@gmail.com>
2021-08-06 18:20:11 +0200

    Updated CORS configuration and added auth api to auth filter

--- xpensetrail-api/src/main/java/com/leratoletsepe/xpensetrail/XpensetrailApiApplication.java
+++ xpensetrail-api/src/main/java/com/leratoletsepe/xpensetrail/XpensetrailApiApplication.java
@@ -1,13 +1,12 @@
 package com.leratoletsepe.xpensetrail;
 
 import com.leratoletsepe.xpensetrail.filters.AuthFilter;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class XpensetrailApiApplication {
@@ -17,16 +16,15 @@ public class XpensetrailApiApplication {
 	}
 
 	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		source.registerCorsConfiguration("/**", config);
-		registrationBean.setFilter(new CorsFilter(source));
-		registrationBean.setOrder(0);
-		return registrationBean;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
 	}
 
 	@Bean
@@ -34,7 +32,7 @@ public class XpensetrailApiApplication {
 		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
 		AuthFilter authFilter = new AuthFilter();
 		registrationBean.setFilter(authFilter);
-		registrationBean.addUrlPatterns("/api/categories/*");
+		registrationBean.addUrlPatterns("/api/categories/*", "/api/user/auth/*");
 		return registrationBean;
 	}
 }
